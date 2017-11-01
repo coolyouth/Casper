137769c8a5c115f47f68839990861a530382a5f0
coolyouth <Bob@ZhangdeMacBook-Pro.local>
2017-11-01 22:08:37 +0800

    add mathjax

--- assets/js/post/custom.js
+++ assets/js/post/custom.js
@@ -7,3 +7,11 @@ $(document).ready(function() {
       hljs.highlightBlock(block);
     });
   });
+  $('.post-full-content').each(function(i,block){
+      if(block.innerHTML.match(/\$\S+\$/)){
+        var mathjax = document.createElement("script");
+        mathjax.src = "/assets/built/js/mathjax.js?config=TeX-AMS_CHTML-full";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(mathjax, s);
+      }
+  })
