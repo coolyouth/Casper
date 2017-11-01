bd9c9ed956104a4f950b5cfad1ea07725c879dc7
coolyouth <Bob@ZhangdeMacBook-Pro.local>
2017-11-01 22:39:53 +0800

    fix bug

--- assets/js/post/custom.js
+++ assets/js/post/custom.js
@@ -19,3 +19,11 @@ $(document).ready(function() {
         }
     });
 });
+$('.post-full-content p').each(function(i,block){
+    if(block.innerHTML.match(/\$\S+\$/)){
+      var mathjax = document.createElement("script");
+      mathjax.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(mathjax, s);
+    }
+})
