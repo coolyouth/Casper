320f5631e8c364e570951737f5afb2dee0f17ded
coolyouth <Bob@ZhangdeMacBook-Pro.local>
2017-11-01 16:52:47 +0800

    concat js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var livereload = require('gulp-livereload');
 var nodemon = require('gulp-nodemon');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
@@ -25,33 +29,58 @@ var nodemonServerInit = function () {
     livereload.listen(1234);
 };
 
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
-});
+gulp.task('build', ['css', 'index-js-minify', 'index-js-concat', 'post-js-minify', 'post-js-concat']);
 
 gulp.task('css', function () {
     var processors = [
         easyimport,
         customProperties,
         colorFunction(),
-        autoprefixer({browsers: ['last 2 versions']}),
+        autoprefixer({ browsers: ['last 2 versions'] }),
         cssnano()
     ];
 
     return gulp.src('assets/css/*.css')
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
         .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
+        .pipe(minifyCss())
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('assets/built/css'))
 });
 
+gulp.task('index-js-minify', function (cb) {
+    var stream = gulp.src('assets/js/index/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js/index/min'))
+    return stream;
+
+})
+gulp.task('index-js-concat', ['index-js-minify'], function (cb) {
+    var stream = gulp.src('assets/js/index/min/*.js')
+        .pipe(concat('index.built.js'))
+        .pipe(gulp.dest('assets/built/js/'))
+    return stream;
+})
+gulp.task('post-js-minify', function (cb) {
+    var stream = gulp.src('assets/js/post/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js/post/min'))
+    return stream;
+
+})
+gulp.task('post-js-concat', ['post-js-minify'], function (cb) {
+    var stream = gulp.src('assets/js/post/min/*.js')
+        .pipe(concat('post.built.js'))
+        .pipe(gulp.dest('assets/built/js/'))
+    return stream;
+
+});
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
 });
 
-gulp.task('zip', ['css'], function() {
+gulp.task('zip', ['css'], function () {
     var targetDir = 'dist/';
     var themeName = require('./package.json').name;
     var filename = themeName + '.zip';
@@ -65,6 +94,4 @@ gulp.task('zip', ['css'], function() {
         .pipe(gulp.dest(targetDir));
 });
 
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
-});
+gulp.task('default', ['build']);
