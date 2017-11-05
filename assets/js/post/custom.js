69f73a7b83b7f0c253b9d91f43150a09c51f21aa
coolyouth <Bob@ZhangdeMacBook-Pro.local>
2017-11-05 14:57:01 +0800

    fix bug

--- assets/js/post/custom.js
+++ assets/js/post/custom.js
@@ -153,72 +153,77 @@ $(document).ready(function () {
             }
             return { close: close, open: open, hold: hold, pause: pause, unpause: unpause }
         })();
+
         $('.floating-header-toc .box').mouseover(function (e) { tocHelper.open() })
         $('.floating-header-toc .box').mouseout(function (e) { tocHelper.close() })
         $('.toc').mouseover(function () { tocHelper.open() })
         $('.toc').mouseout(function () { tocHelper.close() })
-        $('.toc').click(function (event) {
-            var $obj = $(event.target);
-            var id = $obj.attr('id');
-            if (id) { window.scrollTo(0, $("#" + id).offset().top - 40) }
-        })
-        $('.floating-header-toc .box').click(function () { tocHelper.hold() })
     }
+    $('.toc').click(function (event) {
+        var $obj = $(event.target);
+        var id = $obj.attr('id');
+        if (id) { window.scrollTo(0, $("#" + id).offset().top - 40) }
+    })
+    $('.floating-header-toc .box').click(function () { tocHelper.hold() })
+
 
     //End toc
-    var progressBar = document.querySelector('progress');
-    var header = document.querySelector('.floating-header');
-    var title = document.querySelector('.post-full-title');
-    var lastScrollY = window.scrollY;
-    var lastWindowHeight = window.innerHeight;
-    var lastDocumentHeight = $(document).height();
-    var ticking = false;
-    var toc_link = document.querySelectorAll('.toc-link');
-    var header_link_node = document.querySelectorAll('.header-link');
-    var toc_title = document.querySelector('.floating-header-toc #title');
-    var end = document.querySelector('.post-full-footer');
-    var header_link = [];
-    header_link_node.forEach(function (block, i) {
-        header_link.push(block);
-    })
-    header_link.push(end)
-    var header_position = [];
-    header_link.forEach(function (block, i) {
-        header_position.push(block.getBoundingClientRect().top + window.scrollY - 60);
-    })
-    header_position.push(end.getBoundingClientRect().top)
+    if (!isMobile) {
+        var progressBar = document.querySelector('progress');
+        var header = document.querySelector('.floating-header');
+        var title = document.querySelector('.post-full-title');
+        var lastScrollY = window.scrollY;
+        var lastWindowHeight = window.innerHeight;
+        var lastDocumentHeight = $(document).height();
+        var ticking = false;
+        var toc_link = document.querySelectorAll('.toc-link');
+        var header_link_node = document.querySelectorAll('.header-link');
+        var toc_title = document.querySelector('.floating-header-toc #title');
+        var end = document.querySelector('.post-full-footer');
+        var header_link = [];
+        header_link_node.forEach(function (block, i) {
+            header_link.push(block);
+        })
+        header_link.push(end)
+        var header_position = [];
+        header_link.forEach(function (block, i) {
+            header_position.push(block.getBoundingClientRect().top + window.scrollY - 60);
+        })
+        header_position.push(end.getBoundingClientRect().top)
 
-    function onScroll() {
-        lastScrollY = window.scrollY;
-        requestTick();
-    }
-
-    function onResize() {
-        lastWindowHeight = window.innerHeight;
-        lastDocumentHeight = $(document).height();
-        requestTick();
-    }
-
-    function requestTick() {
-        if (!ticking) {
-            requestAnimationFrame(update);
-            ticking = true;
+        function onScroll() {
+            lastScrollY = window.scrollY;
+            requestTick();
         }
-    }
 
-    function update() {
-        var trigger = title.getBoundingClientRect().top + window.scrollY;
-        var triggerOffset = title.offsetHeight + 35;
-        var progressMax = lastDocumentHeight - lastWindowHeight;
-        // show/hide floating header
-        if (lastScrollY >= trigger + triggerOffset) {
-            header.classList.add('floating-active');
-            tocHelper.unpause();
-        } else {
-            header.classList.remove('floating-active');
-            tocHelper.pause();
+        function onResize() {
+            lastWindowHeight = window.innerHeight;
+            lastDocumentHeight = $(document).height();
+            if($(document).width() <= 800){
+                $('.site-header').css('display','block');
+            }
+            requestTick();
         }
-        if (!isMobile) {
+
+        function requestTick() {
+            if (!ticking) {
+                requestAnimationFrame(update);
+                ticking = true;
+            }
+        }
+
+        function update() {
+            var trigger = title.getBoundingClientRect().top + window.scrollY;
+            var triggerOffset = title.offsetHeight + 35;
+            var progressMax = lastDocumentHeight - lastWindowHeight;
+            // show/hide floating header
+            if (lastScrollY >= trigger + triggerOffset) {
+                header.classList.add('floating-active');
+                tocHelper.unpause();
+            } else {
+                header.classList.remove('floating-active');
+                tocHelper.pause();
+            }
             for (var i = 0; i < header_link.length; i++) {
                 var f = i + 1 === header_link.length,
                     l = header_position[i],
@@ -242,10 +247,11 @@ $(document).ready(function () {
             progressBar.setAttribute('value', lastScrollY);
         }
         ticking = false;
+
+
+        window.addEventListener('scroll', onScroll, { passive: true });
+        window.addEventListener('resize', onResize, false);
+
+        update();
     }
-
-    window.addEventListener('scroll', onScroll, { passive: true });
-    window.addEventListener('resize', onResize, false);
-
-    update();
 });
